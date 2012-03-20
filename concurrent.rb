c6e54658fb9607d9645611f5d39787cd6ec8e6bf
Brian Upton <pair+brian@rally.org>
2012-03-20 14:30:57 -0700

    Add concurrency test helper.

---
+++ concurrent.rb
@@ -0,0 +1,36 @@
+require "rubygems"
+require "active_record"
+ActiveRecord::Base.establish_connection(YAML.load_file(File.dirname(__FILE__) + '/db/database.yml')['acts_as_account'])
+
+require "lib/acts_as_account"
+
+puts ARGV.inspect
+
+if ARGV[0]
+  if ARGV[0] == "create"
+    ActsAsAccount::GlobalAccount.create(:name => "incoming").account
+    ActsAsAccount::GlobalAccount.create(:name => "outgoing").account
+    ActsAsAccount::GlobalAccount.create(:name => "revenue").account
+  end
+  puts "spin"
+  until ActsAsAccount::GlobalAccount.find_by_name("wait")
+    #spin...
+  end
+else
+  puts "create"
+  ActsAsAccount::GlobalAccount.create(:name => "wait")
+end
+
+from_account = ActsAsAccount::Account.for("incoming")
+to_account = ActsAsAccount::Account.for("outgoing")
+rev_account = ActsAsAccount::Account.for("revenue")
+
+puts "transactions"
+100.times do
+  ActsAsAccount::Journal.current.transfer(100, from_account, to_account, nil, nil)
+  ActsAsAccount::Journal.current.transfer(5, to_account, rev_account, nil, nil)
+end
+
+puts "bye"
+ActsAsAccount::GlobalAccount.find_by_name("wait").try(:destroy)
+
